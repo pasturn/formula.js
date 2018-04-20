5331a88a2a711e8a83171a5608e21efca32a550d
shiroko <shiroko@webware.co.jp>
2018-04-20 17:20:10 +0900

    fix AVERAGEIF to more closely match Excel functionality

--- test/statistical.js
+++ test/statistical.js
@@ -49,6 +49,8 @@ describe('Statistical', function() {
       [1, 2],
       [3, 4]
     ]).should.approximately(3.5, 1e-9);
+    statistical.AVERAGEIF([3, 4, 4, 5], '4', [1, 2, 3, 4]).should.approximately(2.5, 1e-9);
+    statistical.AVERAGEIF(['aaa', 'bbb', 'bbb', 'ccc'], 'bbb', [1, 2, 3, 4]).should.approximately(2.5, 1e-9);
     statistical.AVERAGEIF([2, 4, 'invalid', 16], '>5').should.equal(error.value);
     statistical.AVERAGEIF().should.equal(error.na);
   });
